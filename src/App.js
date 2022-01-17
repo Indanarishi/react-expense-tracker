9e9c7452c1fb9775006802a025770ddf8464501d
Jundi <indana6969@gmail.com>
2022-01-17 16:18:09 +0700

    finish the ui components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+// components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+
 function App() {
   return (
     <div>
-      
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
