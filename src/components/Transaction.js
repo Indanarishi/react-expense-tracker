0238e60207afb4e6e99270d8b41b97ae52c89644
Jundi <indana6969@gmail.com>
2022-01-17 16:48:38 +0700

    add delete transaction function

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,18 @@
+import { useContext } from "react"
+
+// context
+import { GlobalContext } from "../context/GlobalState"
+
 export const Transaction = ({ transaction }) => {
+
+    const { deleteTransaction } = useContext(GlobalContext)
 
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'} key={transaction.id}>
-            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,12 @@
 const AppReducer = (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => {
+                    return transaction.id !== action.payload
+                })
+            }
         default:
             return state
     }
