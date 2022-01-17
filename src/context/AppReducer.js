bfaf6c5c48eef92de3a3ab17665fabe16fc737cb
Jundi <indana6969@gmail.com>
2022-01-17 16:56:27 +0700

    add add Transaction function

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,13 @@
 const AppReducer = (state, action) => {
     switch(action.type) {
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [
+                    action.payload,
+                    ...state.transactions
+                ]
+            }
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
